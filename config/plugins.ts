fb11eb108cc7f8dae80e1e25878c73a3ad114733
Allen_Yeh <happy1294123@gmail.com>
2024-01-16 22:11:17 +0800

    feat:change CDN to S3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default ({env}) => ({
+export default ({ env }) => ({
   documentation: {
     enabled: true,
     config: {
@@ -9,34 +9,57 @@ export default ({env}) => ({
       },
       'x-strapi-config': {
         // Leave empty to ignore plugins during generation
-        plugins: [ 'upload', 'users-permissions'],
+        plugins: ['upload', 'users-permissions'],
         path: '/documentation',
       },
-      security: [ { bearerAuth: [] } ],
+      security: [{ bearerAuth: [] }],
       servers: [
-      {
-        url: "http://localhost:1337/api",
-        description: "Development server"
-      },
-      {
-        url: "https://dispatch-net.onrender.com/api",
-        description: "remote server"
-      },
-    ],
+        {
+          url: "http://localhost:1337/api",
+          description: "Development server"
+        },
+        {
+          url: "https://dispatch-net.onrender.com/api",
+          description: "remote server"
+        },
+      ],
     },
   },
+  // cloudinary image CDN
+  // upload: {
+  //   config: {
+  //     provider: 'cloudinary',
+  //     providerOptions: {
+  //       cloud_name: env('CLOUDINARY_NAME'),
+  //       api_key: env('CLOUDINARY_KEY'),
+  //       api_secret: env('CLOUDINARY_SECRET'),
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
+
+  // S3 image CDN
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: 'aws-s3',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_BUCKET'),
+        },
       },
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
-  },
+  }
 })
